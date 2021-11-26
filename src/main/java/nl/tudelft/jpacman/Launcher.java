08b72165dbf5c23a6df9f028657daf971e9cff12
Restyled.io <commits@restyled.io>
2021-11-26 16:53:15 +0000

    Restyled by clang-format

--- src/main/java/nl/tudelft/jpacman/Launcher.java
+++ src/main/java/nl/tudelft/jpacman/Launcher.java
@@ -3,7 +3,6 @@ package nl.tudelft.jpacman;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.List;
-
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.board.Direction;
 import nl.tudelft.jpacman.game.Game;
@@ -21,185 +20,177 @@ import nl.tudelft.jpacman.ui.PacManUiBuilder;
 
 /**
  * Creates and launches the JPacMan UI.
- * 
+ *
  * @author Jeroen Roosen
  */
 @SuppressWarnings("PMD.TooManyMethods")
 public class Launcher {
 
-    private static final PacManSprites SPRITE_STORE = new PacManSprites();
+  private static final PacManSprites SPRITE_STORE = new PacManSprites();
 
-    public static final String DEFAULT_MAP = "/board.txt";
-    private String levelMap = DEFAULT_MAP;
+  public static final String DEFAULT_MAP = "/board.txt";
+  private String levelMap = DEFAULT_MAP;
 
-    private PacManUI pacManUI;
-    private Game game;
+  private PacManUI pacManUI;
+  private Game game;
 
-    /**
-     * @return The game object this launcher will start when {@link #launch()}
-     *         is called.
-     */
-    public Game getGame() {
-        return game;
+  /**
+   * @return The game object this launcher will start when {@link #launch()}
+   *         is called.
+   */
+  public Game getGame() { return game; }
+
+  /**
+   * The map file used to populate the level.
+   *
+   * @return The name of the map file.
+   */
+  protected String getLevelMap() { return levelMap; }
+
+  /**
+   * Set the name of the file containing this level's map.
+   *
+   * @param fileName
+   *            Map to be used.
+   * @return Level corresponding to the given map.
+   */
+  public Launcher withMapFile(String fileName) {
+    levelMap = fileName;
+    return this;
+  }
+
+  /**
+   * Creates a new game using the level from {@link #makeLevel()}.
+   *
+   * @return a new Game.
+   */
+  public Game makeGame() {
+    GameFactory gf = getGameFactory();
+    Level level = makeLevel();
+    game = gf.createSinglePlayerGame(level);
+    return game;
+  }
+
+  /**
+   * Creates a new level. By default this method will use the map parser to
+   * parse the default board stored in the <code>board.txt</code> resource.
+   *
+   * @return A new level.
+   */
+  public Level makeLevel() {
+    try {
+      return getMapParser().parseMap(getLevelMap());
+    } catch (IOException e) {
+      throw new PacmanConfigurationException(
+          "Unable to create level, name = " + getLevelMap(), e);
     }
+  }
 
-    /**
-     * The map file used to populate the level.
-     *
-     * @return The name of the map file.
-     */
-    protected String getLevelMap() {
-        return levelMap;
-    }
+  /**
+   * @return A new map parser object using the factories from
+   *         {@link #getLevelFactory()} and {@link #getBoardFactory()}.
+   */
+  protected MapParser getMapParser() {
+    return new MapParser(getLevelFactory(), getBoardFactory());
+  }
 
-    /**
-     * Set the name of the file containing this level's map.
-     *
-     * @param fileName
-     *            Map to be used.
-     * @return Level corresponding to the given map.
-     */
-    public Launcher withMapFile(String fileName) {
-        levelMap = fileName;
-        return this;
-    }
+  /**
+   * @return A new board factory using the sprite store from
+   *         {@link #getSpriteStore()}.
+   */
+  protected BoardFactory getBoardFactory() {
+    return new BoardFactory(getSpriteStore());
+  }
 
-    /**
-     * Creates a new game using the level from {@link #makeLevel()}.
-     *
-     * @return a new Game.
-     */
-    public Game makeGame() {
-        GameFactory gf = getGameFactory();
-        Level level = makeLevel();
-        game = gf.createSinglePlayerGame(level);
-        return game;
-    }
+  /**
+   * @return The default {@link PacManSprites}.
+   */
+  protected PacManSprites getSpriteStore() { return SPRITE_STORE; }
 
-    /**
-     * Creates a new level. By default this method will use the map parser to
-     * parse the default board stored in the <code>board.txt</code> resource.
-     *
-     * @return A new level.
-     */
-    public Level makeLevel() {
-        try {
-            return getMapParser().parseMap(getLevelMap());
-        } catch (IOException e) {
-            throw new PacmanConfigurationException(
-                    "Unable to create level, name = " + getLevelMap(), e);
-        }
-    }
+  /**
+   * @return A new factory using the sprites from {@link #getSpriteStore()}
+   *         and the ghosts from {@link #getGhostFactory()}.
+   */
+  protected LevelFactory getLevelFactory() {
+    return new LevelFactory(getSpriteStore(), getGhostFactory());
+  }
 
-    /**
-     * @return A new map parser object using the factories from
-     *         {@link #getLevelFactory()} and {@link #getBoardFactory()}.
-     */
-    protected MapParser getMapParser() {
-        return new MapParser(getLevelFactory(), getBoardFactory());
-    }
+  /**
+   * @return A new factory using the sprites from {@link #getSpriteStore()}.
+   */
+  protected GhostFactory getGhostFactory() {
+    return new GhostFactory(getSpriteStore());
+  }
 
-    /**
-     * @return A new board factory using the sprite store from
-     *         {@link #getSpriteStore()}.
-     */
-    protected BoardFactory getBoardFactory() {
-        return new BoardFactory(getSpriteStore());
-    }
+  /**
+   * @return A new factory using the players from {@link #getPlayerFactory()}.
+   */
+  protected GameFactory getGameFactory() {
+    return new GameFactory(getPlayerFactory());
+  }
 
-    /**
-     * @return The default {@link PacManSprites}.
-     */
-    protected PacManSprites getSpriteStore() {
-        return SPRITE_STORE;
-    }
+  /**
+   * @return A new factory using the sprites from {@link #getSpriteStore()}.
+   */
+  protected PlayerFactory getPlayerFactory() {
+    return new PlayerFactory(getSpriteStore());
+  }
 
-    /**
-     * @return A new factory using the sprites from {@link #getSpriteStore()}
-     *         and the ghosts from {@link #getGhostFactory()}.
-     */
-    protected LevelFactory getLevelFactory() {
-        return new LevelFactory(getSpriteStore(), getGhostFactory());
-    }
+  /**
+   * Adds key events UP, DOWN, LEFT and RIGHT to a game.
+   *
+   * @param builder
+   *            The {@link PacManUiBuilder} that will provide the UI.
+   */
+  protected void addSinglePlayerKeys(final PacManUiBuilder builder) {
+    builder.addKey(KeyEvent.VK_UP, moveTowardsDirection(Direction.NORTH))
+        .addKey(KeyEvent.VK_DOWN, moveTowardsDirection(Direction.SOUTH))
+        .addKey(KeyEvent.VK_LEFT, moveTowardsDirection(Direction.WEST))
+        .addKey(KeyEvent.VK_RIGHT, moveTowardsDirection(Direction.EAST));
+  }
 
-    /**
-     * @return A new factory using the sprites from {@link #getSpriteStore()}.
-     */
-    protected GhostFactory getGhostFactory() {
-        return new GhostFactory(getSpriteStore());
-    }
+  private Action moveTowardsDirection(Direction direction) {
+    return () -> {
+      assert game != null;
+      getGame().move(getSinglePlayer(getGame()), direction);
+    };
+  }
 
-    /**
-     * @return A new factory using the players from {@link #getPlayerFactory()}.
-     */
-    protected GameFactory getGameFactory() {
-        return new GameFactory(getPlayerFactory());
+  private Player getSinglePlayer(final Game game) {
+    List<Player> players = game.getPlayers();
+    if (players.isEmpty()) {
+      throw new IllegalArgumentException("Game has 0 players.");
     }
+    return players.get(0);
+  }
 
-    /**
-     * @return A new factory using the sprites from {@link #getSpriteStore()}.
-     */
-    protected PlayerFactory getPlayerFactory() {
-        return new PlayerFactory(getSpriteStore());
-    }
+  /**
+   * Creates and starts a JPac-Man game.
+   */
+  public void launch() {
+    makeGame();
+    PacManUiBuilder builder = new PacManUiBuilder().withDefaultButtons();
+    addSinglePlayerKeys(builder);
+    pacManUI = builder.build(getGame());
+    pacManUI.start();
+  }
 
-    /**
-     * Adds key events UP, DOWN, LEFT and RIGHT to a game.
-     *
-     * @param builder
-     *            The {@link PacManUiBuilder} that will provide the UI.
-     */
-    protected void addSinglePlayerKeys(final PacManUiBuilder builder) {
-        builder.addKey(KeyEvent.VK_UP, moveTowardsDirection(Direction.NORTH))
-                .addKey(KeyEvent.VK_DOWN, moveTowardsDirection(Direction.SOUTH))
-                .addKey(KeyEvent.VK_LEFT, moveTowardsDirection(Direction.WEST))
-                .addKey(KeyEvent.VK_RIGHT, moveTowardsDirection(Direction.EAST));
-    }
+  /**
+   * Disposes of the UI. For more information see
+   * {@link javax.swing.JFrame#dispose()}.
+   *
+   * Precondition: The game was launched first.
+   */
+  public void dispose() {
+    assert pacManUI != null;
+    pacManUI.dispose();
+  }
 
-    private Action moveTowardsDirection(Direction direction) {
-        return () -> {
-            assert game != null;
-            getGame().move(getSinglePlayer(getGame()), direction);
-        };
-    }
-
-    private Player getSinglePlayer(final Game game) {
-        List<Player> players = game.getPlayers();
-        if (players.isEmpty()) {
-            throw new IllegalArgumentException("Game has 0 players.");
-        }
-        return players.get(0);
-    }
-
-    /**
-     * Creates and starts a JPac-Man game.
-     */
-    public void launch() {
-        makeGame();
-        PacManUiBuilder builder = new PacManUiBuilder().withDefaultButtons();
-        addSinglePlayerKeys(builder);
-        pacManUI = builder.build(getGame());
-        pacManUI.start();
-    }
-
-    /**
-     * Disposes of the UI. For more information see
-     * {@link javax.swing.JFrame#dispose()}.
-     *
-     * Precondition: The game was launched first.
-     */
-    public void dispose() {
-        assert pacManUI != null;
-        pacManUI.dispose();
-    }
-
-    /**
-     * Main execution method for the Launcher.
-     *
-     * @param args
-     *            The command line arguments - which are ignored.
-     */
-    public static void main(String[] args) {
-        new Launcher().launch();
-    }
+  /**
+   * Main execution method for the Launcher.
+   *
+   * @param args
+   *            The command line arguments - which are ignored.
+   */
+  public static void main(String[] args) { new Launcher().launch(); }
 }
--- src/main/java/nl/tudelft/jpacman/ui/ButtonPanel.java
+++ src/main/java/nl/tudelft/jpacman/ui/ButtonPanel.java
@@ -1,7 +1,6 @@
 package nl.tudelft.jpacman.ui;
 
 import java.util.Map;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,33 +8,33 @@ import javax.swing.JPanel;
 /**
  * A panel containing a button for every registered action.
  *
- * @author Jeroen Roosen 
+ * @author Jeroen Roosen
  */
 class ButtonPanel extends JPanel {
 
-    /**
-     * Default serialisation ID.
-     */
-    private static final long serialVersionUID = 1L;
+  /**
+   * Default serialisation ID.
+   */
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * Create a new button panel with a button for every action.
-     * @param buttons The map of caption - action for each button.
-     * @param parent The parent frame, used to return window focus.
-     */
-    ButtonPanel(final Map<String, Action> buttons, final JFrame parent) {
-        super();
-        assert buttons != null;
-        assert parent != null;
+  /**
+   * Create a new button panel with a button for every action.
+   * @param buttons The map of caption - action for each button.
+   * @param parent The parent frame, used to return window focus.
+   */
+  ButtonPanel(final Map<String, Action> buttons, final JFrame parent) {
+    super();
+    assert buttons != null;
+    assert parent != null;
 
-        for (final Map.Entry<String, Action> entry : buttons.entrySet()) {      
-            final String caption = entry.getKey();      
-            JButton button = new JButton(caption);
-            button.addActionListener(e -> {
-                entry.getValue().doAction();
-                parent.requestFocusInWindow();
-            });
-            add(button);
-        }
+    for (final Map.Entry<String, Action> entry : buttons.entrySet()) {
+      final String caption = entry.getKey();
+      JButton button = new JButton(caption);
+      button.addActionListener(e -> {
+        entry.getValue().doAction();
+        parent.requestFocusInWindow();
+      });
+      add(button);
     }
+  }
 }
